842af147dd4362a7c30c44b00add84312085fae3
bwack <hansdals@yahoo.no>
2018-04-21 11:29:48 +0200

    Delete direct sram write from summer 2017

--- arduino/SD-cart-controller/SD-cart-controller.ino
+++ arduino/SD-cart-controller/SD-cart-controller.ino
@@ -4,19 +4,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define SHIFTREG_RCLK  14
-#define SHIFTREG_OE_N  15
-#define SRAM_OE_N  16
-#define SRAM_WE_N  17
-#define ADR_BUS_EN_N  18
-#define SS_N 10
-
 #define LEDPIN 2
 
-//byte countH=0;
-//byte countL=0;
-//byte buff[16];
-//byte data;
 unsigned char sendbuffer[256];
 
 #define SDCART_CMD_IS_SD_CART_INSERTED    34
@@ -98,8 +87,7 @@ void printDirectory(File dir, int numTabs) {
 
 void setup() {
   // put your setup code here, to run once:
-  //pinMode(LED_BUILTIN, OUTPUT);
-  //TCCR1A = 0;
+
   pinMode(LEDPIN, OUTPUT);
   
   cli();
@@ -121,20 +109,7 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-
   DDRD = 0x00;
-  pinMode(SS, OUTPUT);
-  pinMode(SHIFTREG_RCLK, OUTPUT);
-  pinMode(SHIFTREG_OE_N, OUTPUT);
-  pinMode(SRAM_OE_N, OUTPUT);
-  pinMode(SRAM_WE_N, OUTPUT);
-  pinMode(ADR_BUS_EN_N, OUTPUT);
-  digitalWrite(SHIFTREG_RCLK, HIGH);
-  digitalWrite(SHIFTREG_OE_N, HIGH);
-  digitalWrite(SRAM_OE_N, HIGH);
-  digitalWrite(SRAM_WE_N, HIGH);
-  digitalWrite(SS,LOW);
-  digitalWrite(ADR_BUS_EN_N, HIGH);
 
   SPI.begin();
   SPI.setBitOrder(MSBFIRST); 
@@ -145,6 +120,53 @@ void setup() {
   PORTC = 0xFF;
   DDRD  |= 0B11000000;
   PORTD |= 0B11000000;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (softserial.available()) {
+    // receive command
+    char command = softserial.read();
+    if (command==SDCART_CMD_IS_SD_CART_INSERTED) {
+      send_is_sd_card_inserted();
+    }
+    else if (command==SDCART_CMD_SEND_DIRECTORY_LISTING) {
+      //send_dir_listing();
+    }
+    else if (command==SDCART_CMD_LOAD_ROM) {
+      while( !softserial.available() );
+      char filenumber = softserial.read();
+      //send_rom_file(filenumber);
+      //hang_around_for_next_reset();
+    }
+
+  }
+}
+
+void sendstream(unsigned char length) {
+  while(timeroverflow==0);
+  timeroverflow=0;
+  while(timeroverflow==0);
+  timeroverflow=0;
+  PORTC = 0x00;
+  PORTD &= 0B00111111;
+  for (unsigned char i=0; i<length; i++) {
+    while(timeroverflow==0);
+    timeroverflow=0;
+    PORTC = sendbuffer[i];
+    PORTD &= 0B00111111;
+    PORTD |= sendbuffer[i] & 0B11000000;
+  }
+  while(timeroverflow==0);
+  timeroverflow=0;
+  PORTC = 0xFF;
+  PORTD |= 0B11000000;
+  while(timeroverflow==0);
+  timeroverflow=0;
+} 
+
+// old stuff stored for later.
 
   /*
   File dataFile = SD.open("shark.bin");
@@ -191,116 +213,3 @@ void setup() {
     writesram(count,buff[count]);
   }
 */
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  //delay(2000);
-//  while(1) {
-//    for (byte b=5; b<11; b++) {
-//      sendbuffer[b-5]=b;
-//    }
-//    sendstream(6);
-//    //delay(2000);
-//  } 
-//  while(1) {
-//    delay(2000);
-//    send_is_sd_card_inserted();
-//  }
-
-
-  if (softserial.available()) {
-    // receive command
-    char command = softserial.read();
-    if (command==SDCART_CMD_IS_SD_CART_INSERTED) {
-      send_is_sd_card_inserted();
-    }
-    else if (command==SDCART_CMD_SEND_DIRECTORY_LISTING) {
-      //send_dir_listing();
-    }
-    else if (command==SDCART_CMD_LOAD_ROM) {
-      while( !softserial.available() );
-      char filenumber = softserial.read();
-      //send_rom_file(filenumber);
-      //hang_around_for_next_reset();
-    }
-
-  }
-  
-  /*
-  if(countL==255) {
-    countH++;
-  }
-  countL++;
-  readsram(countL, countH);
-  */
-}
-
-void sendstream(unsigned char length) {
-  while(timeroverflow==0);
-  timeroverflow=0;
-  while(timeroverflow==0);
-  timeroverflow=0;
-  PORTC = 0x00;
-  PORTD &= 0B00111111;
-  for (unsigned char i=0; i<length; i++) {
-    while(timeroverflow==0);
-    timeroverflow=0;
-    PORTC = sendbuffer[i];
-    PORTD &= 0B00111111;
-    PORTD |= sendbuffer[i] & 0B11000000;
-  }
-  while(timeroverflow==0);
-  timeroverflow=0;
-  PORTC = 0xFF;
-  PORTD |= 0B11000000;
-  while(timeroverflow==0);
-  timeroverflow=0;
-} 
-
-void writesram(byte adrL, byte adrH, byte data) {
-  digitalWrite(SHIFTREG_RCLK, LOW);
-  //digitalWrite(SHIFTREG_OE_N, LOW);
-  digitalWrite(SRAM_OE_N, HIGH);
-  SPI.transfer(adrH);
-  SPI.transfer(adrL);
-  digitalWrite(SHIFTREG_RCLK, HIGH);
-  //delay(50);
-  digitalWrite(SHIFTREG_RCLK, LOW);
-  //delay(100);
-  //DDRD=255;
-  PORTD=data;
-  //PORTD=0x50;
-  //delay(1000);
-  //delay(10);
-  digitalWrite(SRAM_WE_N, LOW);
-  digitalWrite(SRAM_WE_N, LOW);
-  //delay(50);
-  digitalWrite(SRAM_WE_N, HIGH);
-  //delay(100);
-  //digitalWrite(SHIFTREG_OE_N, HIGH);
-  //DDRD=0;
-  //delay(50);
-}
-
-void readsram(byte adrL, byte adrH) {
-  digitalWrite(SHIFTREG_RCLK, LOW);
-  digitalWrite(SHIFTREG_OE_N, LOW);
-  digitalWrite(SRAM_OE_N, HIGH);
-  digitalWrite(SRAM_WE_N, HIGH);
-  digitalWrite(SS,LOW);
-  SPI.transfer(adrH);
-  SPI.transfer(adrL);
-  digitalWrite(SHIFTREG_RCLK, HIGH);
-  digitalWrite(SHIFTREG_RCLK, LOW);
-  //digitalWrite(SHIFTREG_OE_N, HIGH);
-  digitalWrite(SRAM_OE_N, LOW);
-  delay(50);
-  digitalWrite(SRAM_OE_N, HIGH);
-  digitalWrite(SHIFTREG_OE_N, HIGH);
-   //delay(200);
-}
-
-
